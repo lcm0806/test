c485d40003ec14a2823178c63d38215c3c966f82
lcm0806 <82375404+lcm0806@users.noreply.github.com>
2022-03-21 10:50:46 +0900

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.myapplication;
 
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                new AlertDialog.Builder(this)
+                        .setTitle("알람 팝업")
+                        .setMessage("MSG\n\nok!!")
+                        .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dlg, int sumthin) {
+                            }
+                        })
+                        .show(); // 팝업창 보여줌
+                break;
+        }
     }
 }
